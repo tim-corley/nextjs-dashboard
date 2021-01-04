e592615275ce02a9226f7baca78c274abfd1bb0b
Tim Corley <tcorley215@gmail.com>
2021-01-03 19:00:48 -0500

    using icons on toggle knob

--- components/toggle.js
+++ components/toggle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Toggle({ switchTheme, theme }) {
@@ -17,9 +17,17 @@ export default function Toggle({ switchTheme, theme }) {
 
           <div className='toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner'></div>
 
-          <div className='toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0'>{`${
-            theme === 'light' ? 'T' : 'D'
-          }`}</div>
+          <div className='toggle__dot absolute w-6 h-6 p-1 bg-white rounded-full shadow inset-y-0 left-0'>
+            {theme === 'light' ? (
+              <span>
+                <FontAwesomeIcon icon={faMoon} />
+              </span>
+            ) : (
+              <span>
+                <FontAwesomeIcon icon={faSun} />
+              </span>
+            )}
+          </div>
         </div>
 
         <div className='ml-3 text-gray-700 font-medium'>Toggle Me!</div>
